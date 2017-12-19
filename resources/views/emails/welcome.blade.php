5f8a036a04d2704600e2abc4b6e4f54451a758c7
shs@cmp-s.com <jainpiyush68@gmail.com>
2017-12-19 16:34:42 +0530

    changes in email template in local

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -4,8 +4,24 @@
 <title>Welcome Email</title>
 </head>
 <body>
-<h2>Welcome to the site {{$user['name']}}</h2>
-<br/>
-Your registered email-id is {{$user['email']}}
+
+Dear {{$user['name']}},<br><br>
+
+Welcome to Health Check SMS, the specialised system to help medical professionals to remind patients about important issues.<br><br>
+
+You can login at: {{url('login')}}<br><br>
+
+Your username is: {{$user['name']}}.<br><br>
+
+Once logged in, you can easily send automated reminders in multiple languages.<br><br>
+
+If you need any help or would like to request a special feature, please let us know.<br><br>
+
+Best regards<br><br>
+
+Health Check SMS<br><br>
+
+www.healthchecksms.com
+<br><br>
 </body>
 </html>
--- resources/views/emails/welcomeStaff.blade.php
+++ resources/views/emails/welcomeStaff.blade.php
@@ -4,8 +4,24 @@
 <title>Welcome Email</title>
 </head>
 <body>
-<h2>Welcome to the site {{$staff['first_name']}} {{$staff['last_name']}}</h2>
-<br/>
-Your registered email-id is {{$staff['email']}}
+
+Dear {{$staff['first_name']}} {{$staff['last_name']}},<br><br>
+
+Welcome to Health Check SMS, the specialised system to help medical professionals to remind patients about important issues.<br><br>
+
+You can login at: {{url('login')}}<br><br>
+
+Your username is: {{$staff['first_name']}} {{$staff['last_name']}}<br><br>
+
+Once logged in, you can easily send automated reminders in multiple languages.<br><br>
+
+If you need any help or would like to request a special feature, please let us know.<br><br>
+
+Best regards<br><br>
+
+Health Check SMS<br><br>
+
+www.healthchecksms.com
+<br><br>
 </body>
 </html>
