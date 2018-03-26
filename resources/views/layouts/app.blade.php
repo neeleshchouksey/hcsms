2a297f7c8eabbfc03cde9fc8ffafa672421556ba
shs@cmp-s.com <jainpiyush68@gmail.com>
2018-03-26 16:57:52 +0530

    changes in menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Styles -->
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-     
+
 <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.css" rel="stylesheet"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -67,7 +67,7 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                      
+                        <li><div id="google_translate_element"></div></li>
                         @auth
                             <li><a href="{{ route('patient.index') }}">Patients</a></li>
                             <li><a href="{{ route('staff.index') }}">Staff</a></li>
@@ -135,7 +135,13 @@
     $('select').select2();
 });
     </script>
-    
+    <script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     @if(request()->is('patient')!=1 && request()->is('staff')!=1)
    <script src="{{ asset('js/a1pp.js') }}"></script>
    @endif
