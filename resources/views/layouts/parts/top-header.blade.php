ae3835f37f535e6708cddbc6cba9344943bfb101
shs@cmp-s.com <jainpiyush68@gmail.com>
2018-04-01 21:34:25 +0530

    changes in footer page

--- resources/views/layouts/parts/top-header.blade.php
+++ resources/views/layouts/parts/top-header.blade.php
@@ -5,9 +5,9 @@
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="top_header_add">
                         <ul>
-                            <li><i class="fa fa-map-marker" aria-hidden="true"></i><span>Address :</span>London UK</li>
-                            <li><i class="fa fa-phone" aria-hidden="true"></i><span>Call us :</span> +44 7929 000 321</li>
-                            <li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:support@mediccomms.com"><span>Email :</span> support@mediccomms.com</a></li>
+                            <li><i class="fa fa-map-marker" aria-hidden="true"></i><span></span>London UK</li>
+                            <li><i class="fa fa-phone" aria-hidden="true"></i><span></span> +44 7929 000 321</li>
+                            <li><i class="fa fa-envelope" aria-hidden="true"></i><a href="mailto:support@mediccomms.com"><span></span> support@mediccomms.com</a></li>
                         </ul>
                     </div>
                     <div class="top_login">
--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -93,13 +93,13 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="vedio_heading_wrapper wow fadeInDown" data-wow-delay="0.5s">
-                        <h1 class="med_bottompadder20">Welcome to Hospital</h1>
+                        <h1 class="med_bottompadder20">VIDEO INFO - COMING SOON</h1>
                         <img src="images/Icon_team.png" alt="line" class="med_bottompadder20">
-                        <p>We Care about everything what you want</p>
+                        <p>Our new video is in progress</p>
                         <h4><a class="popup-youtube" href="https://www.youtube.com/embed/xImpyYRVGOc"><img src="images/Play-Icon.png" alt="Play"> play video</a></h4>
                         <div class="video_btn_wrapper right">
                             <ul>
-                                <li><a class="btn" href="#">About Us</a></li>
+                                <li><a class="btn" href="{{url('/about-us')}}">About Us</a></li>
                             </ul>
                         </div>
                     </div>
