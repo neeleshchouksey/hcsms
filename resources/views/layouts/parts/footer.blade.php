85a0967eb2e956f1940f4d25f923bc68dfbc0105
shs@cmp-s.com <jainpiyush68@gmail.com>
2018-04-01 20:03:36 +0530

    changes in footer page

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -27,8 +27,8 @@
                         <div class="address_main">
                             <div class="footer_widget_add">
                                 <a href=""><img src="{{asset('images/Medic-Comms-Logo-in-White.png')}}" class="img-responsive" alt="footer_logo" /></a>
-                                <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor.</p>
-                                <a href="#">Read More <i class="fa fa-long-arrow-right"></i></a>
+                                <p>Helping healthcare professionals to communicate with patients more effectively.</p>
+                                <a href="{{url('about-us')}}">Read More <i class="fa fa-long-arrow-right"></i></a>
                             </div>
                             <div class="footer_box_add">
                                 <ul>
@@ -46,7 +46,7 @@
                 <div class="row">
                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                         <div class="footer_main_wrapper">
-                           <!-- <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 gallary_response hidden-sm">
+                           <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 gallary_response hidden-sm">
                                 <div class="footer_heading">
                                     <h1 class="med_bottompadder10">Instagram</h1>
                                     <img src="{{asset('images/line.png')}}" class="img-responsive" alt="img" />
@@ -63,21 +63,21 @@
                                      </ul> 
                                     </div>
                                 </div>
-                            </div> -->
+                            </div>
                             <!--footer_2-->
-                            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 respons_footer_nav hidden-xs">
+                            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 respons_footer_nav hidden-xs">
                                 <div class="footer_heading footer_menu">
-                                    <h1 class="med_bottompadder10">Userful</h1>
+                                    <h1 class="med_bottompadder10">Navigate</h1>
                                     <img src="{{asset('images/line.png')}}" class="img-responsive" alt="img" />
                                 </div>
                                 <div class="footer_ul_wrapper">
                                     <ul>
-                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i> <a href="#">home</a></li>
-                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="#">about us</a></li>
-                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="#">services</a></li>
-                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="#">doctors</a></li>
-                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="#">blog</a></li>
-                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="#">contact</a></li>
+                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i> <a href="{{url('/')}}">home</a></li>
+                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="{{url('/about-us')}}">about us</a></li>
+                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="{{url('/service')}}">services</a></li>
+                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="{{url('/doctor')}}">doctors</a></li>
+                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="{{url('/blog')}}">blog</a></li>
+                                        <li><i class="fa fa-caret-right" aria-hidden="true"></i><a href="{{url('/contact-us')}}">contact</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -100,14 +100,14 @@
                         <div class="footer_botm_wrapper">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <div class="bottom_footer_copy_wrapper">
-                                    <span>Copyright © 2018- <a href="#">Life Line</a>Design by <a href="#">Webstrot.</a></span>
+                                    <span>Copyright © 2018- <a href="{{url('/')}}">Medical Communication Services.</a>Design by <a href="#">Webstrot.</a></span>
                                 </div>
                                 <div class="footer_btm_icon">
                                     <ul>
                                         <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://www.linkedin.com/company/mediccomms/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                        <li><a href="https://twitter.com/mediccomms"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                                        <!-- <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li> -->
                                     </ul>
                                 </div>
                             </div>
