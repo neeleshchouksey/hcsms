5351c136d24de24463de47e0e49425a9d404d40a
shs@cmp-s.com <jainpiyush68@gmail.com>
2018-04-02 19:03:05 +0530

    changes in user dashboard

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
+  <div class="med_tittle_section">
+        <div class="med_img_overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="med_tittle_cont_wrapper">
+                        <div class="med_tittle_cont">
+                            <h1>Register</h1>
+                            <ol class="breadcrumb">
+                                <li><a href="{{url('/')}}">Home</a>
+                                </li>
+                                <li>Register</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<div class="dashboard-wrapper">
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -325,6 +345,7 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
 @section('jsscript')
